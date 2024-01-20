11724ef1ff91ff60e7c6ea2c4deaa0bc16de7e6b
Gooxey <dennis.tiderko@gmail.com>
2024-01-20 16:40:09 +0100

    Added binary caches

--- system/default.nix
+++ system/default.nix
@@ -85,6 +85,13 @@
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
+
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
 
     gc = {
