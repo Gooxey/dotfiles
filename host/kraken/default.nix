6e295a42f0008b9dd7929bb5f20bb8aefb59424b
Gooxey <dennis.tiderko@gmail.com>
2024-01-27 16:46:34 +0000

    Modified hardware config of Kraken

--- host/kraken/default.nix
+++ host/kraken/default.nix
@@ -23,9 +23,13 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/nixos";
+      device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
     };
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+    }
   };
 
   swapDevices = [ ];
