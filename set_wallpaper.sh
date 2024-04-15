2b0f32bb3d97d48d3f381e3f3db9f545106ec6e3
dennis <dennis@kraken.localdomain>
2024-04-15 22:39:53 +0200

    Found a nice wallpaper

---
+++ set_wallpaper.sh
@@ -0,0 +1,3 @@
+wallpaper=$(realpath ./wallpaper/gruvbox_astro.jpg)
+gsettings set org.gnome.desktop.background picture-uri file://$wallpaper
+gsettings set org.gnome.desktop.background picture-uri-dark file://$wallpaper
